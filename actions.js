ce4d02d1448364bc411fea7cb73298706ebedc2a
Qbo218 <feb.qbo@gmail.com>
2020-11-25 11:48:27 +0800

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,40 +1,48 @@
-const exec = require('child_process').execSync
-const fs = require('fs')
-const download = require('download')
+const exec = require("child_process").execSync;
+const fs = require("fs");
+const axios = require("axios");
+const smartReplace = require("./smartReplace");
 
 // 公共变量
-const URL = process.env.URL
-const path = "./result.txt";
-const runFile = process.env.RUNFILE;
+const Secrets = {
+    JD_COOKIE: process.env.JD_COOKIE, //cokie,多个用&隔开即可
+    SyncUrl: process.env.SYNCURL, //签到地址,方便随时变动
+    PUSH_KEY: process.env.PUSH_KEY, //server酱推送消息
+    BARK_PUSH: process.env.BARK_PUSH, //Bark推送
+    TG_BOT_TOKEN: process.env.TG_BOT_TOKEN, //TGBot推送Token
+    TG_USER_ID: process.env.TG_USER_ID, //TGBot推送成员ID
+    MarketCoinToBeanCount: process.env.JDMarketCoinToBeans, //京小超蓝币兑换京豆数量
+    JoyFeedCount: process.env.JDJoyFeedCount, //宠汪汪喂食数量
+    FruitShareCodes: process.env.FruitShareCodes, //京东农场分享码
+    Unsubscribe: process.env.UNSUBSCRIBE, //取关商铺
+};
 
-async function downFile() {
-    let file = await download(URL, './')
-    await fs.writeFileSync(`./${runFile}`, file, 'utf8')
+async function changeFiele() {
+    let response = await axios.get(Secrets.SyncUrl);
+    let content = response.data;
+    content = await smartReplace.replaceWithSecrets(content, Secrets);
+    await fs.writeFileSync("./execute.js", content, "utf8");
+    console.log("替换变量完毕");
 }
-async function deleteFile(path) {
-  // 查看文件result.txt是否存在,如果存在,先删除
-  const fileExists = await fs.existsSync(path);
-  // console.log('fileExists', fileExists);
-  if (fileExists) {
-    const unlinkRes = await fs.unlinkSync(path);
-    // console.log('unlinkRes', unlinkRes)
-  }
-}
+
 async function start() {
-    // 下载最新代码
-    await downFile();
-    console.log('下载代码完毕')
-    // 执行
-    await exec(`node ${runFile} >> result.txt`);
-    console.log('执行完毕')
-
-    let content = "";
-    if (fs.existsSync(path)) {
-      content = fs.readFileSync(path, "utf8");
+    console.log(`当前执行时间:${new Date().toString()}`);
+    if (!Secrets.JD_COOKIE) {
+        console.log("请填写 JD_COOKIE 后在继续");
+        return;
     }
-    console.log(content)
-    console.log('运行完成后，删除下载的文件\n')
-    await deleteFile(path);
-    await deleteFile(`./${runFile}`);
+    if (!Secrets.SyncUrl) {
+        console.log("请填写 SYNCURL 后在继续");
+        return;
+    }
+    console.log(`当前共${Secrets.JD_COOKIE.split("&").length}个账号需要签到`);
+    try {
+        await changeFiele();
+        await exec("node execute.js", { stdio: "inherit" });
+    } catch (e) {
+        console.log("执行异常:" + e);
+    }
+    console.log("执行完毕");
 }
-start()
+
+start();
